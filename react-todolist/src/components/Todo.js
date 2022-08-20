ef70935a66a835f022d14d81450611faef72fbd1
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2022-08-20 23:57:24 +0300

    todos added

--- react-todolist/src/components/Todo.js
+++ react-todolist/src/components/Todo.js
@@ -1,8 +1,8 @@
 import React from "react";
-const Todo = () => {
+const Todo = ({text}) => {
     return (
         <div className="todo">
-            <li className="todo-item">Hey</li>
+            <li className="todo-item">{text}</li>
             <button className="complete-btn"><i className="fas fa-check"></i></button>
             <button className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
