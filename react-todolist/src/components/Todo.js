8450b006349c7a3b1b9bd931c6b9c492b49f0a4f
Mira Mansour <81388088+m-mansour1@users.noreply.github.com>
2022-08-26 23:59:28 +0300

    fix

--- react-todolist/src/components/Todo.js
+++ react-todolist/src/components/Todo.js
@@ -1,9 +1,20 @@
+import { Template } from "ejs";
 import React from "react";
-const Todo = ({text, todos, setTodos}) => {
+const Todo = ({text, todo, todos, setTodos}) => {
     //EVENTS 
     const deleteHandler = () => {
-
-    }
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+    const completeHandler = () => {
+        setTodos(todos.map((item) => {
+            if(item.id === todo.id){
+                return{
+                    ...item,completed: !item.completed
+                }
+            }
+            return item;
+        })
+    };
     return (
         <div className="todo">
             <li className="todo-item">{text}</li>
